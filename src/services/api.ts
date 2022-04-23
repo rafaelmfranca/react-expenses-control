4598cc53ac31300f52df31d98aeb3aaf5a9a7450
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-04-23 16:45:57 -0300

    :sparkles: Add exchangeRates api

---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getExchangeRates = async () => {
+  const ENDPOINT = 'https://economia.awesomeapi.com.br/json/all';
+  const { data } = await axios.get(ENDPOINT);
+  delete data.USDT;
+  return data;
+};
+
+getExchangeRates();
