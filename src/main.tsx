8a945ce1562dd1e32a468a84733436aec279a19f
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-04-22 23:57:31 -0300

    :clown_face: Update routes - Fake api

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,46 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          date: new Date(),
+          type: 'expense',
+          title: 'Car',
+          amount: 1500,
+          category: 'Other'
+        },
+        {
+          id: 2,
+          date: new Date(),
+          type: 'income',
+          title: 'Website freelance',
+          amount: 3500,
+          category: 'Salary'
+        }
+      ]
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Lorem ipsum dolor sit amet',
-          amount: 1000,
-          category: 'Lorem ipsum',
-          date: new Date()
-        }
-      ];
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
     });
   }
 });
