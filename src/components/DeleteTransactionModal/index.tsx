ef25783e3e8186ba30888f55a03d919300cd40c9
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-04-25 13:59:50 -0300

    :sparkles: Add delete transaction modal component

---
+++ src/components/DeleteTransactionModal/index.tsx
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react';
+import Modal from 'react-modal';
+import TransactionsContext from '../../contexts/transactions/context';
+import StyledDeleteTransaction from './styles';
+
+// bind modal for acessibility purposes
+Modal.setAppElement('#root');
+
+type DeleteTransactionModalProps = {
+  isOpen: boolean;
+  onRequestClose: () => void;
+  transactionId: string;
+};
+
+const DeleteTransactionModal: React.FC<DeleteTransactionModalProps> = ({
+  isOpen,
+  onRequestClose,
+  transactionId
+}) => {
+  const { deleteTransaction } = useContext(TransactionsContext);
+
+  const handleDeleteTransaction = () => {
+    deleteTransaction(transactionId);
+    onRequestClose();
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <StyledDeleteTransaction>
+        <h2>Delete transaction</h2>
+        <p>Are you sure you want to delete this transaction?</p>
+        <div>
+          <button type="button" onClick={() => onRequestClose()}>
+            Cancel
+          </button>
+          <button type="button" onClick={handleDeleteTransaction}>
+            Delete
+          </button>
+        </div>
+      </StyledDeleteTransaction>
+    </Modal>
+  );
+};
+
+export default DeleteTransactionModal;
---
+++ src/components/DeleteTransactionModal/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+const StyledDeleteTransaction = styled.div`
+  h2 {
+    color: ${(props) => props.theme.colors.textTitle};
+    margin-bottom: 1.5rem;
+  }
+
+  div {
+    margin-top: 2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 0.5rem;
+
+    button {
+      width: 8rem;
+      padding: 0.5rem;
+      font-weight: 500;
+      font-size: 1rem;
+
+      border: none;
+      border-radius: 0.5rem;
+
+      &:first-of-type {
+        background: transparent;
+        transition: background 0.25s linear;
+        color: ${(props) => props.theme.colors.textTitle};
+
+        &:hover {
+          background: ${(props) => props.theme.colors.shapeBorder};
+        }
+      }
+
+      &:nth-of-type(2) {
+        background: var(--red-400);
+        color: white;
+        transition: filter 0.25s linear;
+
+        &:hover {
+          filter: brightness(1.2);
+        }
+      }
+    }
+  }
+`;
+
+export default StyledDeleteTransaction;
