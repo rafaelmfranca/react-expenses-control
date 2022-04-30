7c827f4bc5284c4ad22dda0ee003bfa7109c8043
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-04-30 13:16:13 -0300

    :building_construction: Adapt app to contain routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import useTheme from './hooks/useTheme';
 import lightTheme from './styles/themes/light';
 import darkTheme from './styles/themes/dark';
 import Header from './components/Header';
-import Overview from './components/Overview/index';
+import Overview from './pages/Overview/index';
 import TransactionsProvider from './contexts/transactions';
 import SideMenu from './components/SideMenu';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   const [theme, toggleTheme] = useTheme();
@@ -15,11 +17,21 @@ const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <GlobalStyle />
-      <SideMenu toggleTheme={toggleTheme} theme={theme} />
-      <Header />
-      <TransactionsProvider>
-        <Overview />
-      </TransactionsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/overview"
+            element={
+              <TransactionsProvider>
+                <SideMenu toggleTheme={toggleTheme} theme={theme} />
+                <Header />
+                <Overview />
+              </TransactionsProvider>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
